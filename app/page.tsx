39a6768ba2bec0774fdcff77bbbaa4d92cd28752
Miyuri15 <mlokuhewage15@gmail.com>
2024-09-12 05:40:53 +0530

    Up to Favourites part done

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,23 @@
 import { MapFilterItems } from "@/app/components/MapFilterItems";
 import prisma from "./lib/db";
 import ListingCard from "./components/ListingCard";
-import Link from "next/link";
+import { Suspense } from "react";
+import { SkeletonCard } from "./components/SkeletonCard";
+import { NoItems } from "./components/NoItem";
 
-async function getData(){
+async function getData({
+  searchParams,
+}:{
+  searchParams?:{
+    filter?:string;
+  }
+}){
   const data = await prisma.home.findMany({
     where:{
       addedCategory:true,
       addedDescription:true,
       addedLocation:true,
+      categoryName: searchParams?.filter ?? undefined
     },
     select:{
       photo:true,
@@ -21,13 +30,40 @@ async function getData(){
   return data;
 }
 
-export default async function Home() {
-  const data = await getData()
+export default function Home({
+  searchParams,
+}:{
+  searchParams?:{
+    filter?:string;
+  }
+}) {
   return (
 <div className="container mx-auto px-5 lg:px-10">
   <MapFilterItems/>
+  <Suspense key={searchParams?.filter} fallback={<SkeletonLoading/>}>
+  <ShowItems searchParams={searchParams}/>
+  </Suspense>
+</div>
+  );
+}
 
-    <div className="grid lg:grid-cols-4 sm:grid-cols-2 md:grid-cols-3 gap-8 mt-8" >
+async function ShowItems({
+  searchParams,
+}:{
+  searchParams?:{
+    filter?:string;
+  }
+}
+
+){
+  const data = await getData({searchParams: searchParams})
+
+  return(
+    <>
+    {data.length === 0 ? (
+      <NoItems/>
+    ):(
+      <div className="grid lg:grid-cols-4 sm:grid-cols-2 md:grid-cols-3 gap-8 mt-8" >
       {data.map((item) => (
         <ListingCard key={item.id} 
         description={item.description as string}
@@ -37,6 +73,27 @@ export default async function Home() {
         />
       ))}
     </div>
-</div>
-  );
+  
+    )}
+    </>
+  )
+
+}
+
+function SkeletonLoading(){
+  return(
+    <div className="grid lg:grid-cols-4 sm:grid-cols-2 md:grid-cols-3 gap-8 mt-8">
+      <SkeletonCard/>
+      <SkeletonCard/>
+      <SkeletonCard/>
+      <SkeletonCard/>
+      <SkeletonCard/>
+      <SkeletonCard/>
+      <SkeletonCard/>
+      <SkeletonCard/>
+      <SkeletonCard/>
+      <SkeletonCard/>
+
+    </div>
+  )
 }
